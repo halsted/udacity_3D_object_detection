724a621b10ca8f113f70c58891ac66763b5decaa
halsted <lehalsted@ucdavis.edu>
2022-09-28 17:40:25 -0700

    Add files via upload

--- loop_over_dataset.py
+++ loop_over_dataset.py
@@ -49,21 +49,27 @@ import misc.params as params
 ## Set parameters and perform initializations
 
 ## Select Waymo Open Dataset file and frame numbers
-#data_filename = 'training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord' # Sequence 1
+data_filename = 'training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord' # Sequence 1
 #data_filename = 'training_segment-10072231702153043603_5725_000_5745_000_with_camera_labels.tfrecord' # Sequence 2
-data_filename = 'training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord' # Sequence 3
-show_only_frames = [0, 200] # show only frames in interval for debugging
+#data_filename = 'training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord' # Sequence 3
+show_only_frames = [50,51] # show only frames in interval for debugging
 
 ## Prepare Waymo Open Dataset file for loading
 data_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dataset', data_filename) # adjustable path in case this script is called from another working directory
-model = "darknet"
-sequence = "3"
-results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results/' + model + '/results_sequence_' + sequence + '_' + model)
+
+# Uncomment one of the following 2 lines to choose model (also see line 72)
+#model = "darknet"
+model = "fpn-resnet"
+sequence = "1"
+if model=="darknet":
+    results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results/' + model + '/results_sequence_' + sequence + '_' + model)
+else:
+    results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results/' + model + '/results_sequence_' + sequence + '_' + 'resnet')
 datafile = WaymoDataFileReader(data_fullpath)
 datafile_iter = iter(datafile)  # initialize dataset iterator
 
 ## Initialize object detection
-configs_det = det.load_configs(model_name='darknet') # options are 'darknet', 'fpn_resnet'
+configs_det = det.load_configs(model_name= 'fpn_resnet') # options are 'darknet', 'fpn_resnet'
 model_det = det.create_model(configs_det)
 
 configs_det.use_labels_as_objects = False # True = use groundtruth labels as objects, False = use model-based detection
@@ -80,9 +86,9 @@ camera = None # init camera sensor object
 np.random.seed(10) # make random values predictable
 
 ## Selective execution and visualization
-exec_detection = [] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
+exec_detection = ['bev_from_pcl','detect_objects'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
 exec_tracking = [] # options are 'perform_tracking'
-exec_visualization = ['show_range_image','show_pcl'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
+exec_visualization = ['show_objects_in_bev_labels_in_camera'] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
 exec_list = make_exec_list(exec_detection, exec_tracking, exec_visualization)
 vis_pause_time = 0 # set pause time between frames in ms (0 = stop between frames until key is pressed)
 
@@ -160,7 +166,7 @@ while True:
         ## Performance evaluation for object detection
         if 'measure_detection_performance' in exec_list:
             print('measuring detection performance')
-            det_performance = eval.measure_detection_performance(detections, frame.laser_labels, valid_label_flags, configs_det.min_iou)     
+            det_performance = eval.measure_detection_performance(detections, frame.laser_labels, valid_label_flags, configs_det.min_iou) 
         else:
             print('loading detection performance measures from file')
             det_performance = load_object_from_file(results_fullpath, data_filename, 'det_performance_' + configs_det.arch + '_' + str(configs_det.conf_thresh), cnt_frame)   
@@ -266,7 +272,8 @@ while True:
 
 ## Evaluate object detection performance
 if 'show_detection_performance' in exec_list:
-    eval.compute_performance_stats(det_performance_all, configs_det)
+    #eval.compute_performance_stats(det_performance_all, configs_det)
+    eval.compute_performance_stats(det_performance_all)
 
 ## Plot RMSE for all tracks
 if 'show_tracks' in exec_list:
--- objdet_pcl.py
+++ objdet_pcl.py
@@ -86,6 +86,7 @@ def show_range_image(frame, lidar_name):
     # step 1 : extract lidar data and range image for the roof-mounted lidar
     
     # Load range image (From Example C1-5-1)
+    
     lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0] # get laser data structure from frame
     ri = []
     if len(lidar.ri_return1.range_image_compressed) > 0: # use first response
@@ -126,7 +127,9 @@ def show_range_image(frame, lidar_name):
     # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
     
     range_intensity = np.vstack((img_range, img_intensity))
-    img_range_intensity = range_intensity.astype(np.uint8)
+
+    # For stacked range and intensity image, use next line and comment the following one. For intensity image only, comment next line and uncomment following one.
+    img_range_intensity = range_intensity.astype(np.uint8)    
     #img_range_intensity = img_intensity
     
     # img_range_intensity = [] # remove after implementing all steps
@@ -152,14 +155,22 @@ def bev_from_pcl(lidar_pcl, configs):
     ####### ID_S2_EX1 START #######     
     #######
     print("student task ID_S2_EX1")
+    
+    # Used Exercise C2-3-2 : Transform metric point coordinates to BEV space
 
     ## step 1 :  compute bev-map discretization by dividing x-range by the bev-image height (see configs)
+    bev_discret = (configs.lim_x[1] - configs.lim_x[0]) / configs.bev_height    
 
     ## step 2 : create a copy of the lidar pcl and transform all metrix x-coordinates into bev-image coordinates    
-
+    lidar_pcl_cpy = np.copy(lidar_pcl)
+    lidar_pcl_cpy[:, 0] = np.int_(np.floor(lidar_pcl_cpy[:, 0] / bev_discret))
+    
     # step 3 : perform the same operation as in step 2 for the y-coordinates but make sure that no negative bev-coordinates occur
+    # transform all metrix y-coordinates as well but center the foward-facing x-axis on the middle of the image
+    lidar_pcl_cpy[:, 1] = np.int_(np.floor(lidar_pcl_cpy[:, 1] / bev_discret) + (configs.bev_width + 1) / 2)
 
     # step 4 : visualize point-cloud using the function show_pcl from a previous task
+    show_pcl(lidar_pcl_cpy)
     
     #######
     ####### ID_S2_EX1 END #######     
@@ -170,18 +181,47 @@ def bev_from_pcl(lidar_pcl, configs):
     #######
     print("student task ID_S2_EX2")
 
+    # Used Exercise C2-3-2 : Transform metric point coordinates to BEV space    
+    
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
+    intensity_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
 
     # step 2 : re-arrange elements in lidar_pcl_cpy by sorting first by x, then y, then -z (use numpy.lexsort)
+    idx_height = np.lexsort((-lidar_pcl_cpy[:, 2], lidar_pcl_cpy[:, 1], lidar_pcl_cpy[:, 0]))
+    lidar_pcl_top = lidar_pcl_cpy[idx_height]
 
     ## step 3 : extract all points with identical x and y such that only the top-most z-coordinate is kept (use numpy.unique)
     ##          also, store the number of points per x,y-cell in a variable named "counts" for use in the next task
+    _, idx_height_unique, counts = np.unique(lidar_pcl_top[:, 0:2], axis=0, return_index=True, return_counts=True)
+    lidar_pcl_top = lidar_pcl_top[idx_height_unique]
 
     ## step 4 : assign the intensity value of each unique entry in lidar_top_pcl to the intensity map 
     ##          make sure that the intensity is scaled in such a way that objects of interest (e.g. vehicles) are clearly visible    
     ##          also, make sure that the influence of outliers is mitigated by normalizing intensity on the difference between the max. and min. value within the point cloud
+    
+    # sort points such that in case of identical BEV grid coordinates, the points in each grid cell are arranged based on their intensity
+    lidar_pcl_top[lidar_pcl_top[:,3]>1.0,3] = 1.0
+    idx_intensity = np.lexsort((-lidar_pcl_top[:, 3], lidar_pcl_top[:, 1], lidar_pcl_top[:, 0]))
+    lidar_pcl_top = lidar_pcl_top[idx_intensity]
+
+    # only keep one point per grid cell
+    _, indices = np.unique(lidar_pcl_top[:, 0:2], axis=0, return_index=True)
+    lidar_pcl_int = lidar_pcl_top[indices]
+
+    intensity_map[np.int_(lidar_pcl_int[:, 0]), np.int_(lidar_pcl_int[:, 1])] = lidar_pcl_int[:, 3] / (np.amax(lidar_pcl_int[:, 3])-np.amin(lidar_pcl_int[:, 3]))
 
     ## step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
+
+    temp = 0
+    if temp:        
+        img_intensity = intensity_map * 256
+        img_intensity = img_intensity.astype(np.uint8)
+        while (1):
+            cv2.imshow('img_intensity', img_intensity)
+            if cv2.waitKey(10) & 0xFF == 27:
+                break
+        cv2.destroyAllWindows()
+        
 
     #######
     ####### ID_S2_EX2 END ####### 
@@ -193,21 +233,33 @@ def bev_from_pcl(lidar_pcl, configs):
     print("student task ID_S2_EX3")
 
     ## step 1 : create a numpy array filled with zeros which has the same dimensions as the BEV map
+    height_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
 
     ## step 2 : assign the height value of each unique entry in lidar_top_pcl to the height map 
     ##          make sure that each entry is normalized on the difference between the upper and lower height defined in the config file
     ##          use the lidar_pcl_top data structure from the previous task to access the pixels of the height_map
+    height_map[np.int_(lidar_pcl_top[:, 0]), np.int_(lidar_pcl_top[:, 1])] = lidar_pcl_top[:, 2] / float(np.abs(configs.lim_z[1] - configs.lim_z[0]))
 
     ## step 3 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
 
+    if temp:        
+        img_height = height_map * 256
+        img_height = img_height.astype(np.uint8)
+        while (1):
+            cv2.imshow('img_height', img_height)
+            if cv2.waitKey(10) & 0xFF == 27:
+                break
+        cv2.destroyAllWindows()
+    
+    
     #######
     ####### ID_S2_EX3 END #######       
 
     # TODO remove after implementing all of the above steps
-    lidar_pcl_cpy = []
-    lidar_pcl_top = []
-    height_map = []
-    intensity_map = []
+    #lidar_pcl_cpy = []
+    #lidar_pcl_top = []
+    #height_map = []
+    #intensity_map = []
 
     # Compute density layer of the BEV map
     density_map = np.zeros((configs.bev_height + 1, configs.bev_width + 1))
